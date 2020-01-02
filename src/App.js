5b095481c6ce3601790d51a23adafe13abba1647
GopalaKrishnan.R <gopal.krishnan@gulaq.com>
2020-01-02 13:03:31 +0530

    Test changes.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 
-//  importing manual components
+// importing manual components
 import ProductListComponent from './components/productsList';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <ProductListComponent />
-       </div>
+      </div>
     );
   }
 }
